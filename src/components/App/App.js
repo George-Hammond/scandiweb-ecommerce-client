1674885aac889070b149c84a3e83b556ea187d6b
George N.A. Hammond <91903758+George-Hammond@users.noreply.github.com>
2022-09-14 17:49:48 +0000

    Initial Setup, Create React App

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../Styles/App.css';
+
+
+class App extends React.Component{
+  render(){
+    return(
+      <h1>This is Apps</h1>
+    )
+  }
+}
+
+export default App;
