cb83a1c6fc1ca03c764993798fa42e819481ae71
George N.A. Hammond <91903758+George-Hammond@users.noreply.github.com>
2022-09-14 22:52:56 +0000

    Navigation bar setup 1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import '../../Styles/App.css';
+import NavBar from '../NavigationBar/NavBar';
 
 
 class App extends React.Component{
   render(){
     return(
-      <h1>This is Apps</h1>
+      <main>
+        <h1>This is Apps</h1>
+        <NavBar />
+      </main>
     )
   }
 }
--- src/components/NavigationBar/NavBar.js
+++ src/components/NavigationBar/NavBar.js
@@ -1,2 +1,26 @@
 import React from "react";
-import '../../Styles/NavBar.css'
+import '../../Styles/NavBar.css';
+import GreenBag from '../../../images/green-bag.svg'
+
+class NavBar extends React.Component{
+    render(){
+        return(
+            <nav>
+                <div className="main-nav-container">
+                    <div className='category-name'>                        
+                        <ul>
+                            <li>ALL</li>
+                            <li>CLOTHES</li>
+                            <li>TECH</li>
+                        </ul>        
+                    </div>
+                    <div className='middle-logo'>
+                        <img src={GreenBag} alt="green bag logo" />
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default NavBar;
