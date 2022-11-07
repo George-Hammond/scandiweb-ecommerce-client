361a0ed3302323f0d618f1046e619e6da118ee95
Nii Ayitey Hammond <https://github.com/George-Hammond>
2022-11-07 21:08:45 +0000

    Navigation development III

--- src/components/NavigationBar/NavBar.js
+++ src/components/NavigationBar/NavBar.js
@@ -13,8 +13,44 @@ class NavBar extends React.Component {
             isClothesActive: false,
             isTechActive: false
         }
+        this.navClicked = this.navClicked.bind(this)
+    }
 
-        
+    navClicked = (event) => {
+        const idTag = event.currentTarget;
+        console.log(idTag.id)
+        switch(idTag.id){
+            case "all":
+                this.setState({
+                    isAllActive: true,
+                    isClothesActive: false,
+                    isTechActive: false
+                });
+                break;
+            
+            case "clothes":
+                this.setState({
+                    isAllActive: false,
+                    isClothesActive: true,
+                    isTechActive: false
+                });
+                break;
+            
+            case "tech":
+                this.setState({
+                    isAllActive: false,
+                    isClothesActive: false,
+                    isTechActive: true
+                });
+                break;
+
+            default: 
+            this.setState({
+                isAllActive: true,
+                isClothesActive: false,
+                isTechActive: false
+            });
+        }
     }
     render(){
         return(
@@ -22,17 +58,41 @@ class NavBar extends React.Component {
                 <div className="main-nav-container">
                     <div className='category-name'>                        
                         <ul>
-                            <NavLink to='/'                         
-                            >                                  
+                            <NavLink 
+                                to='/'
+                                id='all'
+                                onClick={this.navClicked}                         
+                                >  
+                            <span
+                                className={this.state.isAllActive ? 'activeClicked' : ''}
+                            >
                                 All
+                            </span>                                
                             </NavLink>
-                            <NavLink to='clothes'                                                                
-                            >
-                                Clothes
+                            
+                            <NavLink 
+                                to='clothes'
+                                id="clothes"
+                                onClick={this.navClicked}                                                                
+                                >
+                                <span
+                                className={this.state.isClothesActive ? 'activeClicked' : ''}
+                                >
+                                    Clothes
+                                </span>
                             </NavLink >
-                            <NavLink to='tech'                                                              
-                            >
-                                Tech
+
+                            
+                            <NavLink 
+                                to='tech'
+                                id="tech"
+                                onClick={this.navClicked}                                                              
+                                >
+                                <span
+                                    className={this.state.isTechActive ? 'activeClicked' : ''}
+                                >                                    
+                                    Tech
+                                </span>
                             </NavLink >
                         </ul>        
                     </div>
